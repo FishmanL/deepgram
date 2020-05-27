0e2693a8e36a95c90210ef29f80471ae1efed4f3
Leor <leor.fishman@gmail.com>
2020-05-27 15:55:37 -0400

    wav processing done

---
+++ wavprocessor.py
@@ -0,0 +1,42 @@
+import wave
+
+
+
+def processfile(binobj):
+    """
+    takes in a wave.read object, outputs a metadata processed version of the object
+    :param binobj: the output of wave.read() on a given file
+    :return: a dict containing metadata and content
+    """
+    retdict = {"nframes": -1, "framerate":-1, "content": bytes(), "seconds":-1, "samplewidth": -1, "numchannels":-1}
+    try:
+        retdict["nframes"] = binobj.getnframes()
+        retdict["framerate"] = binobj.getframerate()
+        retdict["content"] = binobj.readframes(retdict["nframes"])
+        retdict["seconds"] = retdict["nframes"]/retdict["framerate"]
+        retdict["samplewidth"] = binobj.getsampwidth()
+        retdict["numchannels"] = binobj.getnchannels()
+    except Exception as e:
+        print(e)
+    return retdict
+
+
+
+
+
+if __name__ == "__main__":
+    with wave.open("CantinaBand3.wav", "rb") as f:
+        p = processfile(f)
+        print(p)
+        with wave.open("test.wav", "wb") as g:
+            g.setnchannels(p["numchannels"])
+            g.setsampwidth(p["samplewidth"])
+            g.setframerate(p["framerate"])
+            g.writeframes(p["content"])
+    with wave.open("README.md", "rb") as f:
+        print(processfile(f))
+
+
+
+
+
